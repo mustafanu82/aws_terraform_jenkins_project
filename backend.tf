6d575861a9ba2cae9868a5a4340e50e330232a7b
mustafanu82 <82030444+mustafanu82@users.noreply.github.com>
2022-07-17 16:00:46 +0000

    Updated my bucket name

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "sameeduddin-infra-19june21"
+    bucket = "my-terraform-project-bucket12101"
     key    = "aws-jenkins-remote.tfstate"
     region = "us-east-1"
   }
